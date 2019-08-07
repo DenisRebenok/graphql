54d66e6e6c66365fadf19a734f1f1e2f3a4b08e2
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-08-08 00:08:36 +0300

    Search Movies in DB

--- application/src/components/MoviesSearch/MoviesSearch.jsx
+++ application/src/components/MoviesSearch/MoviesSearch.jsx
@@ -5,9 +5,8 @@ import SearchIcon from '@material-ui/icons/Search';
 import withHocs from './MoviesSearchHoc';
 
 class MoviesSearch extends React.Component {
-
   render() {
-    const { classes } = this.props;
+    const { classes, name, handleChange, handleSearch } = this.props;
 
     return (
       <div className={classes.search}>
@@ -15,15 +14,18 @@ class MoviesSearch extends React.Component {
           <SearchIcon />
         </div>
         <InputBase
+          onChange={handleChange('name')}
+          onKeyPress={e => handleSearch(e)}
+          value={name}
           placeholder="Search…"
           classes={{
             root: classes.inputRoot,
-            input: classes.inputInput,
+            input: classes.inputInput
           }}
         />
       </div>
     );
   }
-};
+}
 
 export default withHocs(MoviesSearch);
